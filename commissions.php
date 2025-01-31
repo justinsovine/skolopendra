14f1876bd3e62bc041d0aa8e7ac502f6a55739e5
Justin Sovine <justinsovine@gmail.com>
2025-01-31 00:13:14 -0500

    Updates

--- commissions.php
+++ commissions.php
@@ -1,12 +1,19 @@
 <div class="container mx-auto px-4 mt-10 text-gray-300">
     <div class="max-w-3xl mx-auto">
         <h1 class="text-3xl font-bold text-center md:text-left mb-6 khand-light">Commissions</h1>
+        
         <p class="text-lg leading-relaxed">
             Emily is now available for commissions in the Olympia/Seattle/Portland area! Are you looking for unique, evocative, dark, and surreal art for a special project, locale, or occasion?
             Emily is available for personal commissions in painting and sculpture, murals, cover and promo art for bands and musicians, custom wands and scepters, and more!
         </p>
+        
         <p class="text-lg leading-relaxed mt-4">
-            For more information or to request a commission, please email: <a href="mailto:skolopendraaa.studio@gmail.com" class="text-lime-400">skolopendraaa.studio@gmail.com</a>
+            For more information or to request a commission, please feel free to email me at: 
+            <a href="mailto:skolopendraaa.studio@gmail.com" class="text-lime-400">skolopendraaa.studio@gmail.com</a>
+        </p>
+        
+        <p class="text-lg leading-relaxed mt-6">
+            Be on the lookout for Skolopendra Studio at local markets, festivals, and other events! You can check back with this page to see upcoming events.
         </p>
     </div>
 </div>
