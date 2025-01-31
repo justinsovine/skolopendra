9a16b6d716437e650e4528c0c0c3bad4a04c17fb
Justin Sovine <justinsovine@gmail.com>
2025-01-30 23:22:54 -0500

    Fix nav

--- inc/nav.php
+++ inc/nav.php
@@ -4,11 +4,11 @@
     <!-- Navigation Menu -->
     <nav class="mt-6">
         <ul class="flex justify-center space-x-6 text-xl khand-regular">
-            <li><a href="index.php" class="hover:text-lime-600 transition">Home</a></li>
-            <li><a href="about.php" class="hover:text-lime-600 transition">About</a></li>
-            <li><a href="commissions.php" class="hover:text-lime-600 transition">Commissions</a></li>
-            <li><a href="music.php" class="hover:text-lime-600 transition">Music</a></li>
-            <li><a href="contact.php" class="hover:text-lime-600 transition">Contact</a></li>
+            <li><a href="/" class="hover:text-lime-600 transition">Home</a></li>
+            <li><a href="/about" class="hover:text-lime-600 transition">About</a></li>
+            <li><a href="/commissions" class="hover:text-lime-600 transition">Commissions</a></li>
+            <li><a href="/music" class="hover:text-lime-600 transition">Music</a></li>
+            <li><a href="/contact" class="hover:text-lime-600 transition">Contact</a></li>
         </ul>
     </nav>
 </div>
