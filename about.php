027f99ad5ff096210ef4e6d25dd1cace3273ab1b
Justin Sovine <justinsovine@gmail.com>
2025-01-30 23:55:52 -0500

    Updates

--- about.php
+++ about.php
@@ -2,23 +2,15 @@
     <div class="max-w-3xl mx-auto">
         <h1 class="text-3xl font-bold text-center md:text-left mb-6 khand-light">About the Artist</h1>
 
-        <!-- First paragraph -->
+        <!-- First paragraph with floated image -->
         <div class="relative">
+            <img class="h-auto max-w-full rounded-lg w-full md:w-1/3 md:max-w-[250px] md:float-right md:ml-6 mb-4 md:mb-2" 
+                src="img/031.jpg" alt="Emily Kaelin">
             <p class="text-lg leading-relaxed">
                 Originally from Cincinnati, Ohio, I moved to the Olympia, WA area in September 2024 - a goal I’ve worked towards for over a decade! 
                 Yearning to live closely with the remarkable beauty of the forests local to this part of the country, and the incredible healing, 
                 peace, and awe they provide me - not to mention artistic inspiration - are my primary motivations in relocating to this area.
             </p>
-
-            <!-- Desktop Image (Floats Right) -->
-            <div class="hidden md:block w-1/3 float-right ml-6">
-                <img class="h-auto max-w-full rounded-lg" src="img/031.jpg" alt="Emily Kaelin">
-            </div>
-        </div>
-
-        <!-- Mobile Image (Appears Below the First Paragraph) -->
-        <div class="block md:hidden w-full flex justify-center mt-4">
-            <img class="h-auto max-w-full rounded-lg" src="img/031.jpg" alt="Emily Kaelin">
         </div>
 
         <h1 class="text-3xl font-bold text-center md:text-left mt-8 mb-6 khand-light">Artistic Background</h1>
