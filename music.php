e4abd222a1ccaec337320ab21dc4fbcdeaecae82
Justin Sovine <justinsovine@gmail.com>
2025-01-31 00:09:58 -0500

    Updates

--- music.php
+++ music.php
@@ -1,16 +1,31 @@
+// music.php
 <div class="container mx-auto px-4 mt-10 text-gray-300">
     <div class="max-w-3xl mx-auto">
         <h1 class="text-3xl font-bold text-center md:text-left mb-6 khand-light">VKVEENA - Music</h1>
+        
         <p class="text-lg leading-relaxed">
-            In tandem with my visual art practice, I have also spent over 15 years composing multiple forms of experimental and electronic music.
-            My project, VKVEENA, takes inspiration from industrial, witch house, harsh noise, black metal/doom metal, dark ambient, and other genres to create dark, intense, and
+            In tandem with my visual art practice, I have also spent over 15 years composing multiple forms of experimental and electronic music - an outgrowth of the musical
+            training I received in piano and guitar in my youth. I am currently focusing on songwriting, synths, and vocals in an eclectic music project - VKVEENA (pronounced “ook-VEE-nuh”) - 
+            which takes inspiration from industrial, witch house, harsh noise, black metal/doom metal, dark ambient, and other genres to create dark, intense, and
             experimental storytelling through song and sound.
         </p>
+        
+        <p class="text-lg leading-relaxed mt-4">
+            VKVEENA’s first EP, titled “Annihilation”, is due to be released in the coming months, but the single “Like a Stone” (Audioslave cover) is currently available on Spotify, 
+            Bandcamp, Soundcloud, and YouTube for your listening pleasure!
+        </p>
+        
         <p class="text-lg mt-4">
             <a href="https://open.spotify.com/artist/4F0pNfXaXQjCZPEGUj8uzw" class="text-lime-400">Listen on Spotify</a> |
             <a href="https://vkveena.bandcamp.com/track/like-a-stone" class="text-lime-400">Bandcamp</a> |
             <a href="https://soundcloud.com/vkveena" class="text-lime-400">Soundcloud</a> |
             <a href="https://www.youtube.com/watch?v=NmYum44m-kk" class="text-lime-400">YouTube</a>
+        </p>
+        
+        <p class="text-lg leading-relaxed mt-6">
+            Although VKVEENA has been a solo-project up until now, I am soon to expand my sound through the addition of a guitarist, and I am also looking for a live drummer to 
+            complete the picture! If you’re a local drummer proficient in multiple forms of metal-style drumming - black metal in particular - and love dark and insane music, 
+            please feel free to reach out to me via email! <a href="mailto:skolopendraaa.studio@gmail.com" class="text-lime-400">skolopendraaa.studio@gmail.com</a>
         </p>
     </div>
 </div>
