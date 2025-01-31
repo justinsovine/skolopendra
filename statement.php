3bd26635c43fbe69c576ddec45b8df9bca8bcbd9
Justin Sovine <justinsovine@gmail.com>
2025-01-31 00:05:56 -0500

    Updates

--- statement.php
+++ statement.php
@@ -1,52 +1,52 @@
 <div class="container mx-auto px-4 mt-10 text-gray-300">
     <div class="max-w-3xl mx-auto">
         <h1 class="text-3xl font-bold text-center md:text-left mb-6 khand-light">Artist Statement</h1>
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create because I must, because there is too much within my mind and my heart to be
             contained within my own self. I must express, I must expunge, this filthy excess of
             feeling and fear and passion and desire.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create because I am overwhelmed, I am at the mercy of chaos, and I must develop an
             order to this chaos, to channel it, to make sense of it, to find the most strange beauty in
             it, to create meaning where there may be no meaning.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create because I witness in life all around me the reflection of the same chaos within
             me, the same unfathomable dualities and infinite complexity - outward into the cosmos
             and beyond, and back inwards to the most microcosmic level - in body, mind, and soul.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create so that I don’t lose touch with myself or my being, so that I don’t disconnect and
             dissociate from myself and the rest of existence, to keep the void at bay, and to honor
             the immensely incomprehensible nature of the void.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create so that I don’t suffer alone, so that my shadows can have a voice, so that my
             life has meaning that overcomes any entropy or atrophy I may face.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create to cast a thread into the void in search of another to catch it, to weave of web of
             connection between any who see my work as a door they can enter.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create to honor the profound beauty and terror of nature and the endless cycles of
             creation, destruction, and rebirth, and to channel these same forces within my creative
             work to manifest transformation and transcendence.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create because it is my birthright as a human, and I must resist the countless efforts
             we all endure that attempt to beat our inherent creative nature out of us.
         </p>
 
-        <p class="text-lg leading-relaxed">
+        <p class="text-lg mb-4">
             I create as sacred ritual and primal scream, my offerings a gift of love, and a tribute to
             the void above me, the dirt below me, and the fire within me - and within us all.
         </p>
